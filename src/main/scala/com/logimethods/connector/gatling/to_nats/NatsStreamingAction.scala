de41d184f64eecb588cfc02319c41a89925e48c5
Laurent Magnin <laurent.magnin@logimethods.com>
2016-08-22 12:07:14 -0400

    NatsStreamingAction introduced

---
+++ src/main/scala/com/logimethods/connector/gatling/to_nats/NatsStreamingAction.scala
@@ -0,0 +1,135 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Logimethods
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License (MIT)
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/MIT
+ *******************************************************************************/
+
+// @see https://www.trivento.io/write-custom-protocol-for-gatling/
+package com.logimethods.connector.gatling.to_nats
+
+import akka.actor.{ ActorSystem, Props }
+import io.gatling.commons.stats.{ KO, OK }
+import io.gatling.commons.util.TimeHelper
+import io.gatling.core.CoreComponents
+import io.gatling.commons.stats.{ KO, OK }
+import io.gatling.commons.util.TimeHelper
+import io.gatling.core.CoreComponents
+import io.gatling.core.action.builder.ActionBuilder
+import io.gatling.core.action.{ Action, ActionActor, ExitableActorDelegatingAction }
+import io.gatling.core.config.GatlingConfiguration
+import io.gatling.core.protocol._
+import io.gatling.core.session.Session
+import io.gatling.core.stats.StatsEngine
+import io.gatling.core.stats.message.ResponseTimings
+import io.gatling.core.structure.ScenarioContext
+import io.gatling.jms.action.JmsReqReply._
+
+import scala.concurrent.{ Future, Promise }
+
+import java.util.Properties;
+import io.nats.stan.Connection;
+import io.nats.stan.ConnectionFactory;
+
+object NatsStreamingProtocol {
+  val NatsStreamingProtocolKey = new ProtocolKey {
+
+    type Protocol = NatsStreamingProtocol
+    type Components = NatsStreamingComponents
+
+    def protocolClass: Class[io.gatling.core.protocol.Protocol] = classOf[NatsStreamingProtocol].asInstanceOf[Class[io.gatling.core.protocol.Protocol]]
+
+    def defaultValue(configuration: GatlingConfiguration): NatsStreamingProtocol = throw new IllegalStateException("Can't provide a default value for NatsStreamingProtocol")
+
+    def newComponents(system: ActorSystem, coreComponents: CoreComponents): NatsStreamingProtocol ⇒ NatsStreamingComponents = {
+      natsProtocol ⇒ NatsStreamingComponents(natsProtocol)
+    }
+  }
+}
+
+/** A Gatling Protocol to inject messages into NATS.
+ *  
+ * @see [[https://www.trivento.io/write-custom-protocol-for-gatling/ Write a Custom Protocol for Gatling]] 
+ * @see [[https://github.com/nats-io/jnats/blob/jnats-0.4.1/src/main/java/io/nats/client/ConnectionFactory.java ConnectionFactory.java]]
+ * @see [[http://nats-io.github.io/jnats/io/nats/client/ConnectionFactory.html ConnectionFactory API]]
+ * 
+ * @constructor create a new Protocol defined by connection to a NATS server and a subject.
+ * @param properties defining the parameters of NATS server to connect to. This connection is provided by a `new ConnectionFactory(properties)`
+ * @param subject the subject on which the messages will be pushed to NATS
+ */
+case class NatsStreamingProtocol(clusterID: String, subject: String) extends Protocol {
+    val CLIENT_ID_ROOT = "NatsStreamingProtocol_"
+    val clientID = CLIENT_ID_ROOT + System.identityHashCode(this) + Thread.currentThread().getId() + java.lang.System.currentTimeMillis()
+    val connectionFactory: ConnectionFactory = new ConnectionFactory(clusterID, clientID);
+    val connection: Connection = connectionFactory.createConnection()
+ // var connection: Connection = null
+  
+/*  override def warmUp(): Unit = {
+    val connectionFactory: ConnectionFactory = new ConnectionFactory(properties);
+    connection = connectionFactory.createConnection()
+  }*/
+}
+
+case class NatsStreamingComponents(natsProtocol: NatsStreamingProtocol) extends ProtocolComponents {
+
+  def onStart: Option[Session ⇒ Session] = None
+  def onExit: Option[Session ⇒ Unit] = None
+}
+
+object NatsStreamingCall {
+  def apply(messageProvider: Object, protocol: NatsStreamingProtocol, system: ActorSystem, statsEngine: StatsEngine, next: Action) = {
+    val actor = system.actorOf(Props(new NatsStreamingCall(messageProvider, protocol, next, statsEngine)))
+    new ExitableActorDelegatingAction(genName("natsCall"), statsEngine, next, actor)
+  }
+
+}
+
+class NatsStreamingCall(messageProvider: Object, protocol: NatsStreamingProtocol, val next: Action, statsEngine: StatsEngine) extends ActionActor {
+
+  override def execute(session: Session): Unit = {
+    protocol.connection.publish(protocol.subject, messageProvider.toString().getBytes())
+    
+    next ! session
+  }
+}
+
+/** A Gatling ActionBuilder to inject messages into NATS.
+ * 
+ * Possible usage:
+ * {{{
+ *     val natsScn = scenario("NATS call").exec(NatsStreamingBuilder(new ValueProvider()))
+ * }}}
+ * {{{
+ * class ValueProvider {
+ *   val incr = 10
+ *   val basedValue = 100 -incr
+ *   val maxIncr = 50
+ *   var actualIncr = 0
+ *   
+ *   override def toString(): String = {
+ *     actualIncr = (actualIncr % (maxIncr + incr)) + incr
+ *     (basedValue + actualIncr).toString()
+ *   }
+ * }
+ * }}}
+ *  
+ * @see [[https://www.trivento.io/write-custom-protocol-for-gatling/ Write a Custom Protocol for Gatling]] 
+ * @constructor create a new NatsStreamingBuilder that will emit messages into NATS.
+ * @param messageProvider  the provider of the messages to emit. The actual message will be the output of the toString() method applied to this object
+ * (which could be a simple String if the message doesn't have to change over time). 
+ */
+case class NatsStreamingBuilder(messageProvider: Object) extends ActionBuilder {
+  def natsProtocol(protocols: Protocols) = protocols.protocol[NatsStreamingProtocol].getOrElse(throw new UnsupportedOperationException("NatsStreamingProtocol Protocol wasn't registered"))
+
+  private def components(protocolComponentsRegistry: ProtocolComponentsRegistry): NatsStreamingComponents =
+    protocolComponentsRegistry.components(NatsStreamingProtocol.NatsStreamingProtocolKey)
+
+  override def build(ctx: ScenarioContext, next: Action): Action = {
+    import ctx._
+    val statsEngine = coreComponents.statsEngine
+
+    val natsComponents = components(protocolComponentsRegistry)
+    NatsStreamingCall(messageProvider, natsComponents.natsProtocol, ctx.system, statsEngine, next)
+  }
+}
---
+++ src/test/scala/com/logimethods/connector/gatling/to_nats/NatsStreamingActionTest.scala
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Logimethods
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the MIT License (MIT)
+ * which accompanies this distribution, and is available at
+ * http://opensource.org/licenses/MIT
+ *******************************************************************************/
+
+package com.logimethods.connector.gatling.to_nats
+
+import akka.actor.{ActorRef, Props}
+import io.gatling.core.Predef._
+import io.gatling.core.action.builder.ActionBuilder
+
+import scala.concurrent.duration._
+import java.util.Properties
+
+class NatsStreamingActionTest extends Simulation {
+  
+  val clusterID = "test-cluster"
+  val natsProtocol = NatsStreamingProtocol(clusterID, "TestingSubject")
+  
+  val natsScn = scenario("NATS call").exec(NatsStreamingBuilder("Hello from Gatling!"))
+ 
+  setUp(
+    natsScn.inject(constantUsersPerSec(15) during (1 minute))
+  ).protocols(natsProtocol)
+}
